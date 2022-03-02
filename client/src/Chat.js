628507e5b6c9b12cd810cccb8004c43529674056
Simão Freire <simao@pbhub.com.br>
2022-03-01 23:35:52 -0300

    Added styles to the application

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 
 export default function Chat({ socket, username, room }) {
 	const [currentMessage, setCurrentMessage] = useState("");
+	const [messageList, setMessageList] = useState([]);
 
 	const sendMessage = async () => {
 		if (currentMessage !== "") {
@@ -13,23 +15,53 @@ export default function Chat({ socket, username, room }) {
 			};
 
 			await socket.emit("send_message", messageData);
+			setMessageList((list) => [...list, messageData]);
+			setCurrentMessage("");
 		}
 	};
 
 	useEffect(() => {
 		socket.on("receive_message", (data) => {
-			console.log(data);
+			setMessageList((list) => [...list, data]);
 		});
 	}, [socket]);
 
 	return (
-		<div>
+		<div className="chat-window">
 			<div className="chat-header">
 				<p>Live Chat</p>
 			</div>
-			<div className="chat-body"></div>
+
+			<div className="chat-body">
+				<ScrollToBottom className="message-container">
+					{messageList.map((messageContent, index) => {
+						return (
+							<div className="message" key={index} id={username === messageContent.author ? "you" : "other"}>
+								<div>
+									<div className="message-content">
+										<p>{messageContent.message}</p>
+									</div>
+									<div className="message-meta">
+										<p>{messageContent.time}</p>
+										<p>{messageContent.author}</p>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</ScrollToBottom>
+			</div>
+
 			<div className="chat-footer">
-				<input type="text" placeholder="Hey..." onChange={(e) => setCurrentMessage(e.target.value)} />
+				<input
+					type="text"
+					value={currentMessage}
+					placeholder="Hey..."
+					onChange={(e) => setCurrentMessage(e.target.value)}
+					onKeyPress={(e) => {
+						e.key === "Enter" && sendMessage();
+					}}
+				/>
 				<button onClick={sendMessage}>&#9658;</button>
 			</div>
 		</div>
